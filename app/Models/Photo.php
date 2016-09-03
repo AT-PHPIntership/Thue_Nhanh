283abb548463007208eb2d1d9ab6b85144eee4a5
Son Tran <sontd.it@gmail.com>
2016-09-03 20:43:33 +0700

    Setup repository, translate language and storage disk

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
 
-class Photo extends Model
+class Photo extends Model implements Transformable
 {
+    use TransformableTrait;
+
     protected $table = 'photos';
 
     /**
--- config/app.php
+++ config/app.php
@@ -159,6 +159,8 @@ return [
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
 
+        Collective\Html\HtmlServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
     ],
 
     /*
@@ -205,7 +207,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
-
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
